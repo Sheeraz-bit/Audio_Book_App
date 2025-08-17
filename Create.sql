8208a269b2682129514480ee56f8852c34a0a986
Abdul Nabi Sheeraz <sheeraz842122@gmail.com>
2025-08-17 22:03:39 +0530

    Create Create.sql

---
+++ Create.sql
@@ -0,0 +1,17 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(50) DEFAULT 'USER'
+);
+CREATE TABLE audiobooks (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255),
+    author VARCHAR(255),
+    narrator VARCHAR(255),
+    genre VARCHAR(100),
+    cover_url VARCHAR(500),
+    file_url VARCHAR(500),
+    duration INT
+);
